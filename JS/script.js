dcd424a73d4441dfbe298dad5b22df171d880552
Niloy <118054257+Kamrul-Hasan-Niloy@users.noreply.github.com>
2023-01-30 00:11:59 +0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+$('.testimonial-slider').slick({
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
